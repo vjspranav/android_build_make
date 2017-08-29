6ef7b4b72e981c45bfa064c05e7b7bfa7a6d3838
xyyx <xyyx@mail.ru>
2017-08-29 18:48:09 +0800

    Nitrogen art

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -482,6 +482,19 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    build = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+
+    script.Print("- Nitrogen Project ---------------------------");
+    script.Print("*  * *** *** **** **** **** *** *  *  **** ***");
+    script.Print("** *  *   *  *  * *  * *    *   ** *  *  * *");
+    script.Print("* **  *   *  **** *  * * ** *** * **  *  * ***");
+    script.Print("*  *  *   *  * *  *  * *  * *   *  *  *  *   *");
+    script.Print("*  * ***  *  * *  **** **** *** *  *  **** ***");
+    script.Print("- By xyyx -------------------- Android 8.0.0 -");
+    script.Print(" Compiled: %s "%(build));
+    script.Print(" For: %s   "%(model));
+    script.Print("----------------------------------------------");
   recovery_mount_options = OPTIONS.info_dict.get("recovery_mount_options")
 
   script.ShowProgress(system_progress, 0)
