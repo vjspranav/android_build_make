03ab9aada0b575c9d3dc558a65ee7783c302a4a5
Nick Reuter <nreuter85@gmail.com>
2017-09-03 06:05:12 -0500

    soong: pass the qcom hal paths to soong
    Change-Id: Ie75e0a33172ac4008765b2d212c4de17dcb875e0

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -75,7 +75,10 @@ $(SOONG_VARIABLES): FORCE
 	echo ''; \
 	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),';  \
 	echo '    "BoardUsesQCOMHardware":  $(if $(BOARD_USES_QCOM_HARDWARE),true,false),';  \
-	echo '    "TargetUsesQCOMBsp":  $(if $(TARGET_USES_QCOM_BSP),true,false)';  \
+	echo '    "TargetUsesQCOMBsp":  $(if $(TARGET_USES_QCOM_BSP),true,false),';  \
+	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
+	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
+	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)"';  \
 	echo '}') > $(SOONG_VARIABLES_TMP); \
 	if ! cmp -s $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); then \
 	  mv $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); \
