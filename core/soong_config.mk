682b4c0a61b238db79c11caf99169a1010c90976
Shekhawat2 <anandsingh215@yahoo.com>
2018-01-12 08:32:38 +0530

    soong_config: Add TARGET_USES_NON_TREBLE_CAMERA product variable

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -85,6 +85,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Device_uses_hwc2": $(if $(filter true,$(TARGET_USES_HWC2)),true,false),'; \
 	echo '    "Override_rs_driver": "$(OVERRIDE_RS_DRIVER)",'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
 	echo '    "Treble": $(if $(filter true,$(PRODUCT_FULL_TREBLE)),true,false),'; \
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
 	echo ''; \
